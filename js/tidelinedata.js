7ecad34f68173d8a03ffb62e17312c313cda243a
franck <franck@diabeloop.fr>
2020-08-04 08:32:48 +0200

    Deduplicate PAs based on EventIDs

--- js/tidelinedata.js
+++ js/tidelinedata.js
@@ -229,6 +229,16 @@ function TidelineData(data, opts) {
     }
   };
 
+  this.deduplicatePhysicalActivities = function (data = []) {
+    var physicalActivity = _.groupBy(_.filter( data,  {type: 'physicalActivity'}), 'eventId');
+    _.forEach(physicalActivity, function(value, key) {
+      physicalActivity[key] = _.orderBy(value, ['deviceTime'],['desc']);
+    })
+    this.physicalActivities = _.map(physicalActivity, function(value) {
+      return value[0];
+    });
+  };
+  
   this.checkTimezone = function() {
     if (!Array.isArray(this.grouped.upload)) {
       return;
@@ -420,6 +430,9 @@ function TidelineData(data, opts) {
     // get DeviceParameters
     this.setDeviceParameters(this.data);
 
+    // get PhysicalActivities
+    this.deduplicatePhysicalActivities(data);
+  
     // Timezone change events (for tooltips)
     this.checkTimezone();
 
@@ -732,6 +745,12 @@ function TidelineData(data, opts) {
   this.setDeviceParameters(data);
 
   endTimer('deviceEvents');
+
+  startTimer('deduplicatePhysicalActivities');
+
+  this.deduplicatePhysicalActivities(data);
+
+  endTimer('deduplicatePhysicalActivities');
 
   this.setBGPrefs();
 
